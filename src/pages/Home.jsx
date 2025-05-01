3a8e10bdeeb7179d366e6763feda6d23e3ff134e
onni82 <onni.bucht@me.com>
2025-05-01 22:31:41 +0200

    Added home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </>
+    );
+};
+
+export default Home;
