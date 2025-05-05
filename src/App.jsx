4240cc43fb185b5f077831e262a69aed42b858a5
onni82 <onni.bucht@me.com>
2025-05-05 20:20:18 +0200

    Added new pages and added in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
@@ -8,6 +11,9 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Portfolio />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/resume" element={<Resume />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,8 @@
+const Resume = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Resume;
