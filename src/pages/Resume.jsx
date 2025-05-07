af468790670681efdcf3f62e1212371faefa8520
onni82 <onni.bucht@me.com>
2025-05-07 22:18:10 +0200

    Updated Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,74 @@
 const Resume = () => {
     return (
-        <>
-        </>
+        <div className="resume">
+            <section>
+                <h2>Experience</h2>
+
+                <div className="job">
+                    <h3>Administrative Assistant and Video Editor</h3>
+                    <p><strong>Exclusive Maintenance</strong></p>
+                    <p>October 2022 – March 2024</p>
+                    <ul>
+                        <li>Overseeing daily and annual document flow</li>
+                        <li>Planning, organizing, and posting company social media</li>
+                    </ul>
+                </div>
+
+                <div className="job">
+                    <h3>Office Representative</h3>
+                    <p><strong>Volkswagen</strong></p>
+                    <p>May 2022 – October 2023</p>
+                    <ul>
+                        <li>Managing client, schedules, and meetings</li>
+                        <li>Organize company records</li>
+                    </ul>
+                </div>
+
+                <div className="job">
+                    <h3>Assistant Film Editor</h3>
+                    <p><strong>Visual Arcade</strong></p>
+                    <p>June 2020 – July 2022</p>
+                    <ul>
+                        <li>File organization and distribution</li>
+                        <li>Time management</li>
+                    </ul>
+                </div>
+
+                <div className="job">
+                    <h3>Customer Service Representative</h3>
+                    <p><strong>Rona</strong></p>
+                    <p>June 2020 – May 2022</p>
+                    <ul>
+                        <li>Manage online requests</li>
+                        <li>Resolving customer complaints and concerns</li>
+                    </ul>
+                </div>
+            </section>
+            <section>
+                <h2>Education</h2>
+
+                <div className="job">
+                    <h3>Bachelor of Film and Television Production</h3>
+                    <p><strong>Sheridan College</strong></p>
+                    <p>Proficiency in picture editing and visual effects with:</p>
+                    <ul>
+                        <li>Adobe Suite</li>
+                        <li>Avid Media Composer</li>
+                        <li>Maya</li>
+                    </ul>
+                </div>
+
+                <div className="job">
+                    <h3>Honours Graduate</h3>
+                    <p><strong>Philip Pocock CSS</strong></p>
+                    <p>Proficiency/certification:</p>
+                    <ul>
+                        <li>Extended French Certification</li>
+                        <li>Proficiency in Animation and Video Production</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
     );
 };
 
