ee5ef25a7369541eade754cbe72b727b1dfede03
onni82 <onni.bucht@me.com>
2025-05-07 21:48:25 +0200

    Updated Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,13 @@
 const Contact = () => {
     return (
-        <>
-        </>
+        <div className="contact">
+            <p>
+                <a href="mailto:thaishull@gmail.com">thaishull@gmail.com</a>
+            </p>
+            <p>
+                <a href="tel:+16479815736">+1 (647) 981-5736</a>
+            </p>
+        </div>
     );
 };
 
