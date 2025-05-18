4ee506dc34fcc303ba2d4d8cc548b47dc8054ec1
onni82 <onni.bucht@me.com>
2025-05-18 17:02:21 +0200

    adde about class and changed center-container p to about p

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className="center-container">
+        <div className="center-container about">
             <p>I'm a video editor based in Ontario with a deep appreciation for storytelling and the collaborative nature of filmmaking. I bring a strong sense of organization, attention to detail, and creative vision to every project. I specialize in post-production using Avid Media Composer, Adobe Creative Suite, and Maya to support projects that require 3D elements or visual effects.</p>
             <p>Beyond the editing bay, I'm passionate about travel, nature, and connecting with people from all walks of life. Drawing inspiration from stories people share and the world around us - fuels my creativity and helps shape the way I approach each project.</p>
             <p>If this approach feels right for you, reach out about your project.</p>
