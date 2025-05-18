8057b3e75b4a560da1788bdc4efd0d0fc2af3746
onni82 <onni.bucht@me.com>
2025-05-18 17:10:53 +0200

    removed .contact class

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 const Contact = () => {
     return (
-        <div className="contact">
-            <p>
+        <div className="center-container">
+            <p className="center">
                 <a href="mailto:thaishull@gmail.com">thaishull@gmail.com</a>
             </p>
-            <p>
+            <p className="center">
                 <a href="tel:+16479815736">+1 (647) 981-5736</a>
             </p>
         </div>
