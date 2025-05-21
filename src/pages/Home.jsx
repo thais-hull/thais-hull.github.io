f276373adfad7731acfc60feb2d45b3669557a28
onni82 <onni.bucht@me.com>
2025-05-21 18:55:45 +0200

    added youtube class for embedded youtube player

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@
 const Portfolio = () => {
     return (
         <div className="videos">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/uD3T-VSnpGY?si=vb4Ihj36aMpkKQCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe className="video youtube" src="https://www.youtube.com/embed/uD3T-VSnpGY?si=vb4Ihj36aMpkKQCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             {/* {videos.map((video, index) => (
                 <div className="video" key={index}>
                     <video
